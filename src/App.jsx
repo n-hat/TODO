e4b367fed7535da0bb58402d00b9415d9aa1a121
Nam Nhat Hoang <nhat@Nams-MacBook-Pro.local>
2026-02-20 00:34:02 -0500

    Added Create and Read Functionality with commenting to understand how things work.

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+// Importing useState allows us to use states.
+// This is important because states allow us to maintain data from
+// a previous render while re-rendering.
+import { useState } from 'react'
+
+function App() {
+
+  // Creates todos array (list of things to do).
+  // The todos array starts empty.
+  // setTodos is a function that is a byproduct
+  // of creating todos.
+  // The purpose of setTodos is to update todos
+  // when the user adds a todo.
+  // The state then renders after running setTodos.
+  const [todos, setTodos] = useState([])
+
+  // Creats inputValue as an String.
+  // inputValue starts out as an empty String
+  // in the useState argument. ('')
+  // setInputValue is a byproduct of creating inputValue.
+  // The purpose of setInputValue is to let the user 
+  // input a value that will be stored in inputValue.
+  // The state then renders after setInputValue is called.
+  const [inputValue, setInputValue] = useState('')
+
+  // Create a function to add user input TODOS to todos.
+  // Declares a function named addTodo with no parameters.
+  // Directly accesses todos and inputValue from the component.
+  // [...] creates a new array
+  // ...todos - the spread operator which spreads all existings todos into the new array
+  // , inputValue appends the new input value to todos.
+  // Each time the function is called, a new todos array is created where the contents
+  // of the previous todos array is put into the new one.
+  // setInputValue('') clears the text box so the user can add a new todo after submitting a todo.
+  function addTodo() {
+    setTodos([...todos, inputValue])
+    setInputValue('')
+  }
+
+  return (
+    <div>
+      <h1>My Todo List</h1>
+      {/* Create a bar to enter input values.
+          Input is a self-closing tag so it can't have any children. */}
+
+      {/* Establishes inputValue as a controlled variable
+            that can be used only in the return statement. */}
+
+      {/* e is automatically passed in by the browser when an event happens.
+            The browser creates it and hands it to your function as a param. 
+            You could call it anything.
+            Uses setInputValue to get the current text in the input (e.target.value). 
+            React then re-renders, input value is updated, then the input shows the new text. */}
+      <input
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+
+        // if Enter key is pressed it will call addTodo
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') addTodo()
+        }}
+      />
+
+      {/* Create a button to submit the input value to todos. 
+          Adds special syntax so that when the button is clicked it calls addTodo. */}
+      <button onClick={addTodo}>Add Todo</button>
+
+      {/* Display the todos list 
+          Use the map method. */}
+      <ul>
+        {todos.map((todo, index) => (
+            <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
